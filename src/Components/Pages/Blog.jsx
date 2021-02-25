469ef42ef9905e0ad0e37fc2b21ed5083cf6cf7d
omon490 <omonbekmurodov@gmail.com>
2021-02-25 21:09:14 +0500

    add blog jsx

--- src/Components/Pages/Blog.jsx
+++ src/Components/Pages/Blog.jsx
@@ -1,6 +1,8 @@
 import "./Blog.css"
 import {useLanguage} from '../../Context/Language'
 import Localization from '../../Localization'
+import { Link } from "react-router-dom"
+import { useState, useEffect } from "react"
 
 function Blog() {
 
@@ -10,24 +12,51 @@ function Blog() {
 
   const languageChange = e => setLanguage(e.target.value)
 
+  const [data, setData] = useState({
+
+		loading: true,
+		error: null,
+		data: [],
+	})
+
+  useEffect(() => {
+
+		fetch("https://jsonplaceholder.typicode.com/posts")
+		.then(response => response.json())
+		.then(data => {
+
+			setData({ loading: false, data: data, })
+		})
+		.catch(error => {
+			setData({ error: "xatolik yuz berdi" })
+		})
+
+	}, [])
+
   return (
     <>
-    <section id="contact-form">
-    <div className="container">
-      <h1 className="contact-heading">
-        <span className="contact-heading-text">{TEXT.contactMe}</span>{TEXT.me}
-      </h1>
-      <p className="contact-text">{TEXT.contactMeText}</p>
-      <form className="form" action="https://echo.htmlacademy.ru" method="POST">
-        <input className="input" type="text" name="name" placeholder={TEXT.contactName} required />
-        <input className="input" type="email" name="email" placeholder={TEXT.contactEmail} required />
-        <textarea className="textarea" name="message" cols="30" rows="5" placeholder={TEXT.contactMessage}></textarea>
-        <button className="button hvr-buzz" type="submit">{TEXT.contactSubmit}</button>
-      </form>
-    </div>
-  </section>
+		<ul>
+		{ data.loading && <>Loading...</> }
+		{ data.error && <>{data.error}</> }
+		{ !data.loading &&
+    <>
+			<ul className="blog-list">
+			{
+				data.data.map(post => {
 
-    </>
+					return <li className="blog-item" key={post.id}>
+
+					{/* <a href={'/post/' + post.id}>{post.title}</a> */}
+          <Link to="/post/2">{post.title}TEST</Link>
+
+					</li>
+				})
+			}
+			</ul>
+
+			</>}
+			</ul>
+			</>
   )
 }
 
